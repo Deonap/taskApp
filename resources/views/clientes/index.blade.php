1a2a91a8dcc853f8b9bddeb3514243aa084b757b
Nuno Maia <Deonap4216@gmail.com>
2024-03-27 15:59:26 +0000

    Changed formatting on actionButtons in clientes.index

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,65 +1,75 @@
 <x-app-layout>
-<div class="flex-1 bg-gray-200 p-6">
-    <div class="max-w-4xl mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="header">
-            <h2 class="text-lg font-semibold text-gray-700">Lista de Clientes</h2>
-        </div>
-        <div class="table-responsive">
-            <table class="table-auto w-full text-left whitespace-no-wrap">
-                <thead>
-                    <tr class="bg-blue-100">
-                        <th class="px-4 py-2">Nome</th>
-                        <th class="px-4 py-2">Email</th>
-                        <th class="px-4 py-2">Telefone</th>
-                        <th class="px-4 py-2">Ações</th>
-                    </tr>
-                </thead>
-                <tbody class="bg-white divide-y">
-                    @foreach ($clientes as $cliente)
-                    <tr>
-                        <td class="border px-4 py-2">{{ $cliente->nome }}</td>
-                        <td class="border px-4 py-2">{{ $cliente->email }}</td>
-                        <td class="border px-4 py-2">{{ $cliente->telefone }}</td>
-                        <td class="border px-4 py-2">
-                        
-                            <div class="flex items-center space-x-4 text-sm">
-                                <!-- Botão Editar -->
-                                <a href="{{ route('clientes.edit', $cliente->id) }}" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                      </svg>
-                                      
-                                </a>
-                        
-                                <!-- Botão Visualizar -->
-                                <a href="{{ route('clientes.show', $cliente->id) }}" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-blue-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="View">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                      </svg>
-                                      
-                                </a>
-                        
-                                <!-- Botão Excluir -->
-                                @if(auth()->user() && auth()->user()->tipo == 'admin')
-                                <form action="{{ route('clientes.destroy', $cliente->id) }}" method="POST" onsubmit="return confirm('Tem certeza que deseja excluir este cliente?')">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-red-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                          </svg>
-                                          
-                                    </button>
-                                </form>
-                                @endif
-                            </div>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+    <div class="flex-1 bg-gray-200 p-6">
+        <div class="max-w-4xl mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="header">
+                <h2 class="text-lg font-semibold text-gray-700">Lista de Clientes</h2>
+            </div>
+            <div class="table-responsive">
+                <table class="table-auto w-full text-left whitespace-no-wrap">
+                    <thead>
+                        <tr class="bg-blue-100">
+                            <th class="px-4 py-2">
+                                Nome
+                            </th>
+                            <th class="px-4 py-2">
+                                Email
+                            </th>
+                            <th class="px-4 py-2">
+                                Telefone
+                            </th>
+                            <th class="px-4 py-2 text-center">
+                                Ações
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y">
+                        @foreach ($clientes as $cliente)
+                            <tr>
+                                <td class="border px-4 py-2">
+                                    {{ $cliente->nome }}
+                                </td>
+                                <td class="border px-4 py-2">
+                                    {{ $cliente->email }}
+                                </td>
+                                <td class="border px-4 py-2">
+                                    {{ $cliente->telefone }}
+                                </td>
+                                <td class="border px-4 py-2">
+                                    <div class="flex justify-center items-center space-x-4">
+                                        <!-- Botão Visualizar -->
+                                        <a href="{{ route('clientes.show', $cliente->id) }}" class="px-2 py-2 text-sm leading-5 text-blue-600 dark:text-gray-400 focus:shadow-outline-gray" aria-label="View" title="Ver">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                            </svg>
+                                        </a>
+
+                                        <!-- Botão Editar -->
+                                        <a href="{{ route('clientes.edit', $cliente->id) }}" class="px-2 py-2 text-sm leading-5 text-purple-600 dark:text-gray-400 focus:shadow-outline-gray" aria-label="Edit" title="Editar">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                            </svg>
+                                        </a>
+                                
+                                        <!-- Botão Excluir -->
+                                        @if(auth()->user() && auth()->user()->tipo == 'admin')
+                                        <form action="{{ route('clientes.destroy', $cliente->id) }}" method="POST" onsubmit="return confirm('Tem certeza que deseja excluir este cliente?')">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="px-2 py-2 leading-5 text-red-600 dark:text-gray-400 focus:shadow-outline-gray" aria-label="Delete" title="Apagar">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                </svg>
+                                            </button>
+                                        </form>
+                                        @endif
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
 </x-app-layout>
