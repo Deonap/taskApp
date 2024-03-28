cca4dadc6f3b0a212b0c359d080802118bb80ba7
Nuno Maia <Deonap4216@gmail.com>
2024-03-28 10:04:23 +0000

    Cosmetic changes in clientes.create

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -1,33 +1,65 @@
-<x-app-layout>
-
-    <div class="flex-1 bg-gray-200 p-6">
-        <div class="max-w-lg mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <form action="{{ route('clientes.store') }}" method="POST">
-                @csrf
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="nome">
-                        Nome
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nome" type="text" placeholder="Nome" name="nome">
+<html lang='en'>
+    <head>
+        <style>
+            label{
+                display: block;
+                font-size: 0.875rem;
+                line-height: 1.25rem;
+                margin-bottom: 0.5rem;
+                font-weight: 700;
+                 color: rgb(0 40 91);
+            }
+        </style>
+    </head>
+    <body>
+        <x-app-layout>
+            <div class="flex-1 m-20">
+                <div class="flex justify-between items-center mb-4">
+                    <div class="flex items-center text-darkBlue">
+                        <h2 class="text-xl font-black ">
+                            Clientes >
+                        </h2>
+                        <div class="ml-2">
+                            Adicionar
+                        </div>
+                    </div>
                 </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-                        Email
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" name="email">
+                <div class="flex justify-between items-center mb-4">
+                    <form action="{{ route('clientes.store') }}" method="POST">
+                        @csrf
+                        <div class="bg-white shadow-md rounded px-8  py-6 mb-4 border">
+                            <div class="mb-4">
+                                <div class="flex items-center space-x-3">
+                                    <label for="nome">
+                                        Nome
+                                    </label>
+                                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nome" autocomplete='off' type="text" name="nome">
+                                </div>
+                            </div>
+                            <div class="mb-4 flex items-center space-x-3">
+                                <div class="flex items-center space-x-3">
+                                    <label for="email">
+                                        Email
+                                    </label>
+                                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" autocomplete='off' type="email" name="email">
+                                </div>
+                                <div class="flex items-center space-x-3">    
+                                    <label for="telefone">
+                                        Telefone 
+                                    </label>
+                                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="telefone" autocomplete='off' type="text" name="telefone">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-row-reverse mb-4 px-8">
+                            <button class="bg-darkBlue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                                Guardar
+                            </button>
+                        </div>
+                        
+                    </form>
                 </div>
-                <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="telefone">
-                        Telefone
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="telefone" type="text" placeholder="Telefone" name="telefone">
-                </div>
-                <div class="flex items-center justify-between">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                        Adicionar
-                    </button>
-                </div>
-            </form>
-        </div>
-    </div>
-</x-app-layout>
+            </div>
+        </x-app-layout>
+    </body>
+</html>
