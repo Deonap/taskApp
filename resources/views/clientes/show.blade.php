faeb58896e0a832878ee5f3af09ce33419630de5
NunoMaia <Deonap4216@gmail.com>
2024-05-16 18:21:31 +0100

    Started standardizing clientes.show

--- resources/views/clientes/show.blade.php
+++ resources/views/clientes/show.blade.php
@@ -110,7 +110,7 @@
                                 </thead>
                                 <tbody id="projetosAbertosTbody">
                                     @foreach($projetosAbertos as $projeto)
-                                        <tr class="{{!$loop->last ? 'border-b-4 border-b-[#d1d1d1]' : ''}} border-b-4 border-b-[#d1d1d1]">
+                                        <tr class="border-b-4 border-b-[#d1d1d1]">
                                             <td>
                                                 <div class="flex items-end">
                                                     <div>
@@ -191,14 +191,14 @@
                                                             </form>
                                                         @endforeach
                                                     </div>
-                                                    <div class="my-0 mx-3 {{$projeto->users->count() == $colaboradores->count() ? 'hidden' : ''}}">
+                                                    {{-- <div class="my-0 mx-3 {{$projeto->users->count() == $colaboradores->count() ? 'hidden' : ''}}">
                                                         <button id="{{$projeto->id}}"class="btn-adicionar-colaborador">
                                                             <!-- + -->
                                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
                                                                 <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clip-rule="evenodd" />
                                                             </svg>                                                                      
                                                         </button>
-                                                    </div>
+                                                    </div> --}}
                                                 </div>
                                             </td>
                                             <td class="text-center">
@@ -210,7 +210,7 @@
                                                     @endforeach
                                                 </div>
                                             </td>
-                                            <td class="">
+                                            <td>
                                                 <div class="flex justify-end pr-[3rem] space-x-3">
                                                     <a href="{{ route('projetos.edit', $projeto->id) }}" title="Editar">
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-darkBlue hover:text-blue-700">
@@ -230,7 +230,7 @@
                                             </td>
                                         </tr>
                                     @endforeach
-                                    <tr class="" id="newProjectRow">
+                                    <tr class="hidden" id="newProjectRow">
                                         <form method="POST" action="{{ route('projetos.store') }}" autocomplete="off">
                                         @csrf
                                             <input class="hidden" name="cliente_id" value="{{$cliente->id}}">
@@ -408,16 +408,14 @@
                                         <th scope="col" class="w-[0%]">
 
                                         </th>
-                                        @if(auth()->user() && auth()->user()->tipo == 'admin')
-                                            <th class="text-center w-[10%]">
-                                                Ações
-                                            </th>
-                                        @endif
+                                        <th class="w-[10%] text-right pr-[5rem]">
+                                            Ações
+                                        </th>
                                     </tr>
                                 </thead>
-                                <tbody class="divide-y divide-gray-200">
+                                <tbody>
                                     @foreach($projetosConcluidos as $projeto)
-                                        <tr>
+                                        <tr class="border-b-4 border-b-[#d1d1d1]">
                                             <td>
                                                 <div class="flex items-end">
                                                     <div>
@@ -512,24 +510,23 @@
                                                             $bgColor = 'redStatus';
                                                         }
                                                     ?>
-                                                    <div class="bg-{{$bgColor}} rounded-full size-6 m-auto">
-                                                    </div>
+                                                    {{-- <div class="bg-{{$bgColor}} rounded-full size-6 m-auto">
+                                                    </div> --}}
                                                 </div>
                                             </td>
                                             <td>
                                             </td>
-                                            @if(auth()->user() && auth()->user()->tipo == 'admin')
                                             <td>
-                                                <div class="flex justify-center space-x-3">
+                                                <div class="flex justify-end pr-[3rem] space-x-3">
                                                     <a href="{{ route('projetos.edit', $projeto->id) }}" title="Editar">
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-darkBlue hover:text-blue-700">
                                                             <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                                         </svg>
                                                     </a>
-                                                    <form action="{{ route('projetos.destroy', $projeto->id) }}" title="Remover">
+                                                    <form action="{{ route('projetos.destroy', $projeto->id) }}" title="Remover" onsubmit="return confirm('Tem a certeza?')">
                                                         @csrf
                                                         @method('DELETE')
-                                                        <button type="submit" onclick="return confirm('Tem a certeza?')">
+                                                        <button type="submit" {{$hasPermissions ? "" : 'disabled'}} class="disabled:cursor-not-allowed">
                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-red-700 hover:text-red-500">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                             </svg>
@@ -537,7 +534,6 @@
                                                     </form>
                                                 </div>
                                             </td>
-                                            @endif
                                         </tr>
                                     @endforeach
                                 </tbody>
